806ece12ea40ffd8b44ceb9827ae89b19f9c6144
Matthew Buckman <mbuck25@gmail.com>
2019-08-26 07:59:01 -0500

    Add lambda code to start/hibernate instances

---
+++ hibernate-ec2/start-ec2.py
@@ -0,0 +1,12 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    for instance in ec2.instances.all():
+        for tag in (instance.tags or []):
+            if tag['Key'] == 'Hibernate' and \
+            tag['Value'] == 'True':
+                instance.start()
+                print('Starting Instance: ' + instance.id)
+                break
